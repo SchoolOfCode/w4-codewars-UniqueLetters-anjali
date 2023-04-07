0ee86ea7c0beb6a28455983d231b5c18704c3fce
anjaliruth <ruthanjali0211@gmail.com>
2023-04-07 11:30:47 +0800

    Tests passed, ready to play

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 /* 
 👉 Write your kata here!
-*/
+*/// Unique Letters: Write a function that takes in a string and returns True if all the letters in the string are unique and False otherwise. For example, "hello" should return False because it contains the letter "l" twice, while "world" should return True.
 
 //👉 Write the function your CodeWarriors will start with below here:
+export function uniqueLetters(string) {
+    //👉 Write your code here:
+      }
+      
